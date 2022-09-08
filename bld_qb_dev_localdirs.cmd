fe7ba637d76227d0dc34d1802c02c153570ae15d
itLezy <itLezy@users.noreply.github.com>
2022-09-08 09:08:41 +0200

    ok

--- bld_qb_dev_localdirs.cmd
+++ bld_qb_dev_localdirs.cmd
@@ -5,7 +5,7 @@ CD ..
 
 CALL "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 
-
+SET CMAKE_PREFIX_PATH=c:\prj\p2p\libtorrent-2.0.7-install
 SET CMAKE=c:\prj\vcpkg\downloads\tools\cmake-3.22.2-windows\cmake-3.22.2-windows-i386\bin\cmake.exe
 SET CMAKE_TOOLCHAIN_FILE_Z=C:/prj/vcpkg/scripts/buildsystems/vcpkg.cmake
 
